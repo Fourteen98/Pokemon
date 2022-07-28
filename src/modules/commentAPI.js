ecc7e71628f6156932f978cd477fcb2538044247
aiziji <aletv1501@gmail.com>
2022-07-28 15:07:00 -0400

    implement functionality for commenst

---
+++ src/modules/commentAPI.js
@@ -0,0 +1,29 @@
+
+const key = '0rucBNxYXY17tjHN06zy';
+const apiUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${key}/comments/`
+
+const postComment = async (data) => {
+    return await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+  }
+
+  const getComment = async (id) => {
+    try {
+      return await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${key}/comments/?item_id=${id}`)
+    .then(response => response.json())
+    .then(data => data)
+    } catch (error) {
+      return console.log(error)
+    }
+  }
+
+
+export default {
+  postComment,
+  getComment
+}
--- src/modules/commentPopup.js
+++ src/modules/commentPopup.js
@@ -1,21 +1,53 @@
-const createCommentPopup = (pokemon) => {
+import commentAPI from "./commentAPI";
+
+const updateUl = (data, ul, h3) => {
+  ul.innerHTML = ''
+  if(data.length > 0) {
+    data.forEach(child => {
+      let commentLi = document.createElement('li');
+      
+      const usernameP = document.createElement('p')
+      usernameP.textContent = `user: ${child.username}`
+      
+      const commentP = document.createElement('p')
+      commentP.textContent = `coment: ${child.comment}`
+
+      
+      const dateP = document.createElement('p')
+      dateP.classList.add('ul-date');
+      dateP.textContent = `${child.creation_date}`
+  
+      commentLi.appendChild(usernameP)
+      commentLi.appendChild(commentP)
+      commentLi.appendChild(dateP)
+      ul.appendChild(commentLi)
+    });
+  }
+  h3.textContent = `coments (${data.length})`
+}
+
+const createCommentPopup = async (pokemon) => {
   const commentWrapper = document.querySelector('.modal-wrapper');
   commentWrapper.style.zIndex = 99;
 
   const commentModal = document.createElement('div')
   commentModal.classList.add('modal','comments');
   
-  // create comments
+  const h3 = document.createElement('h3');
+  h3.classList.add('modal-h3');
+  h3.textContent = `coments`
+  // h3.classList.add('form-p');
+  
+  let commentsData = await commentAPI.getComment(pokemon.id)
   const commentUl = document.createElement('ul');
-  let example = [
-    'comment 1',
-    'comment 2'
-  ]
-  example.forEach(child => {
-    let commentLi = document.createElement('li');
-    commentLi.textContent = child
-    commentUl.appendChild(commentLi)
-  });
+  commentUl.classList.add('modal-ul');
+  if(commentsData['error']) {
+    h3.textContent = 'coments (0)'
+  }else {
+    updateUl(commentsData, commentUl, h3)
+  }
+  // create comments
+
   
   // create form
   const form = document.createElement('form');
@@ -30,10 +62,10 @@ const createCommentPopup = (pokemon) => {
   inputName.classList.add('form-input');
   form.appendChild(inputName)
   
-  const inputcomments = document.createElement('textArea')
-  inputcomments.placeholder = 'Your comment of the pokemon'
-  inputcomments.classList.add('form-textarea');
-  form.appendChild(inputcomments)
+  const inputComments = document.createElement('textArea')
+  inputComments.placeholder = 'Your comment of the pokemon'
+  inputComments.classList.add('form-textarea');
+  form.appendChild(inputComments)
   
   const inputSubmit = document.createElement('input')
   inputSubmit.setAttribute('type', 'submit')
@@ -41,8 +73,24 @@ const createCommentPopup = (pokemon) => {
   inputSubmit.classList.add('form-submit');
   form.appendChild(inputSubmit)
   
-  inputSubmit.addEventListener('click', (e) => {
+  inputSubmit.addEventListener('click', async (e) => {
     e.preventDefault()
+    if(inputName.value.trim() == '') {
+      inputName.placeholder = 'please enter your name'
+    }else if( inputComments.value.trim() == '') {
+      inputComments.placeholder = 'please enter your descripion or comment'
+    }else {
+      await commentAPI.postComment({
+        "item_id": pokemon.id,
+        "username": inputName.value,
+        "comment":  inputComments.value
+      });
+      let newData = await commentAPI.getComment(pokemon.id)
+    updateUl(newData, commentUl, h3)
+
+    inputComments.value = '';
+    inputName.value = '';
+    }
   })
 
   // delete the modal
@@ -55,6 +103,7 @@ const createCommentPopup = (pokemon) => {
 
   // appending the childs in order
   
+  commentModal.appendChild(h3)
   commentModal.appendChild(commentUl)
   commentModal.appendChild(form)
   commentWrapper.appendChild(commentModal)
