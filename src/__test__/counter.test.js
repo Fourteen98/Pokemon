a59eaa90bfb8b5eb11d608a3186769408cd58edc
Fourteen98 <emuhiydeen6@gmail.com>
2022-07-28 22:11:31 +0000

    fix test with jest

--- src/__test__/counter.test.js
+++ src/__test__/counter.test.js
@@ -1,7 +1,7 @@
-import {mockApi, mockLen} from "./__mock__/api-mock.test.js";
+import { mockApi, mockLen } from './__mock__/api-mock.js';
 
 describe('counter for api and pokemon card', () => {
   test('verify number of items on board is equal api limit', () => {
-    expect(mockApi).toBe(mockLen);
+    expect(mockApi).toStrictEqual(mockLen);
   });
 });
